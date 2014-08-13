c86dbbc03b7b54ed606f1b75f457f0d71d4d8ffd
Jon Daniel <binarycleric@gmail.com>
2014-08-13 13:12:47 -0400

    Giving the board some walls and a roof

--- intro-to-physics/main.lua
+++ intro-to-physics/main.lua
@@ -10,33 +10,52 @@ function love.load()
   world = love.physics.newWorld(0, 9.81 * 64, true)
 
   -- creating the ground
-  local ground_body = love.physics.newBody(world, 800/2, 650-50/2)
-  local ground_shape = love.physics.newRectangleShape(650, 50)
-  local ground_fixture = love.physics.newFixture(ground_body, ground_shape)
-  ground_body:setUserData("ground")
+  buildGround()
 
   -- creating the ball
   local ball_body = love.physics.newBody(world, 800/2, 650/2, "dynamic")
   local ball_shape = love.physics.newCircleShape(20)
   local ball_fixture = love.physics.newFixture(ball_body, ball_shape)
-  ball_fixture:setRestitution(0.9)
+  ball_fixture:setRestitution(0.85)
   ball_body:setUserData("ball")
 
   -- creating some blocks
   local block1_body = love.physics.newBody(world, 200, 550, "dynamic")
-  local block1_shape = love.physics.newRectangleShape(0, 0, 50, 100)
+  local block1_shape = love.physics.newRectangleShape(50, 100)
   local block1_fixture = love.physics.newFixture(block1_body, block1_shape, 5) 
   block1_body:setUserData("block")
 
   local block2_body = love.physics.newBody(world, 200, 400, "dynamic")
-  local block2_shape = love.physics.newRectangleShape(0, 0, 100, 30)
+  local block2_shape = love.physics.newRectangleShape(100, 30)
   local block2_fixture = love.physics.newFixture(block2_body, block2_shape, 2)
   block2_body:setUserData("block")
 
   local block3_body = love.physics.newBody(world, 450, 600, "dynamic")
-  local block3_shape = love.physics.newRectangleShape(0, 0, 50, 125)
-  local block3_fixture = love.physics.newFixture(block3_body, block3_shape, 2)
+  local block3_shape = love.physics.newRectangleShape(50, 125)
+  local block3_fixture = love.physics.newFixture(block3_body, block3_shape, 1)
   block3_body:setUserData("block")
+end
+
+function buildGround()
+  local body = love.physics.newBody(world, 800/2, 650-50/2)
+  local shape = love.physics.newRectangleShape(650, 50)
+  local fixture = love.physics.newFixture(body, shape)
+  body:setUserData("ground")
+
+  local body = love.physics.newBody(world, 800/2, 0)
+  local shape = love.physics.newRectangleShape(700, 25)
+  local fixture = love.physics.newFixture(body, shape)
+  body:setUserData("ground")
+
+  local body = love.physics.newBody(world, 25, 25)
+  local shape = love.physics.newRectangleShape(50, 815)
+  local fixture = love.physics.newFixture(body, shape)
+  body:setUserData("ground")
+
+  local body = love.physics.newBody(world, 775, 25)
+  local shape = love.physics.newRectangleShape(50, 815)
+  local fixture = love.physics.newFixture(body, shape)
+  body:setUserData("ground")
 end
 
 function love.update(dt)
